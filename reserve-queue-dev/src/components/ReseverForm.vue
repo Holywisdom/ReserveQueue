2308918cb73d54ca35268e41b10281d1630228bc
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2018-02-07 01:46:16 +0700

    set style

--- reserve-queue-dev/src/components/ReseverForm.vue
+++ reserve-queue-dev/src/components/ReseverForm.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="bg">
-    <div id="inputForm">
-      <v-input id="inputForm" placeholder="Name"> </v-input>
-      <v-input id="inputForm" placeholder="Phone Number"> </v-input>
-      <v-input id="inputForm" placeholder="Seat Number"> </v-input>
-      <v-input id="inputForm" placeholder="Seat Number"> </v-input>
-      <v-input id="inputForm" placeholder="Time Reserve"> </v-input>
-      <v-input id="inputForm" placeholder="Note"> </v-input>
-      <v-button id="button" type="primary" size="large">{{message}}</v-button>
-    </div>
+  <div class="bg">
+    <h1>Resever Queue</h1>
+    <section class="inputForm">
+      <v-input class="inputFormList" size="large" placeholder="Name"> </v-input>
+      <v-input class="inputFormList" size="large" placeholder="Phone Number"> </v-input>
+      <v-input class="inputFormList" size="large" placeholder="Seat Number"> </v-input>
+      <v-input class="inputFormList" size="large" placeholder="Time Reserve"> </v-input>
+      <v-input class="inputFormList" type="textarea" :rows="4" size="large" placeholder="Note"> </v-input>
+      <v-button class="button" type="primary" size="large">{{message}}</v-button>
+    </section>
   </div>
 
 </template>
@@ -17,13 +17,13 @@ export default {
   name: 'ReseverForm',
   data () {
     return {
-      message: 'Hello Vue!'
+      message: 'Payment'
     }
   }
 }
 </script>
 <style scoped>
-#bg {
+.bg {
   background-image: url("../assets/stanford-smith-369293.jpg") ;
   background-position: center center;
   background-repeat: no-repeat;
@@ -33,8 +33,31 @@ export default {
   flex-direction: column;
 }
 
-inputForm {
-  width: 50%;
+.inputForm {
+  width: 35%;
   text-align: center;
+  align-self: center;
+  margin-top: 2% ;
 }
+
+.inputFormList{
+  width: 100% ;
+  margin: 15px 0 0 0;
+}
+
+.button {
+  margin: 15px 0 0 0 ;
+}
+
+.magin {
+  height: 100px;
+}
+
+h1 {
+  margin: 50px 0 0 0 ;
+  color: #ffffff;
+  text-align: center;
+  font-size: 50px;
+}
+
 </style>
