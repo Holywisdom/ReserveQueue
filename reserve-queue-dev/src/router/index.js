de8cc58028598d024dfe336583679dd7b9fc7496
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2018-03-11 01:09:59 +0700

    delete helloworld path

--- reserve-queue-dev/src/router/index.js
+++ reserve-queue-dev/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import ReseverForm from '@/components/ReseverForm'
 import CreateTable from '@/components/CreateTable'
 import ListTable from '@/components/ListTable'
