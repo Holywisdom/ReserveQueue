6e4f6fa62a8156d8db36dc5cd6f00b7adce48929
Thanongkiat Tamtai <tt@20scoops.net>
2018-04-24 20:12:39 +0700

    Remove is-primary class

--- reserve-queue-dev/src/components/TestData.vue
+++ reserve-queue-dev/src/components/TestData.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <section class="hero is-primary">
+    <section class="hero">
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
